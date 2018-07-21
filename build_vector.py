14bfa04a88bc46bb733a939586c2bd992b899786
RahulKrishnan1443 <41118002+RahulKrishnan1443@users.noreply.github.com>
2018-07-21 09:51:15 -0400

    n*300-dimensional word vectors based on n tokens

--- build_vector.py
+++ build_vector.py
@@ -5,14 +5,14 @@ import numpy as np
 from src.word2vec import *
 from src.build_data import *
 
-tokens = ['A', 'ABQ', 'C', 'D']
-vocab = {'A': [1, 2, 3, 4], 'B': [100, 200, 300, 400], 'Q':[.1, .2, .3, .4]}
-vocab['UNK'] = np.random.random(4)
+# tokens = ['RZYK', 'KZ&','ABCBC', 'ABCHG']
+# vocab = {'A': [1, 2, 3, 4], 'B': [100, 200, 300, 400], 'Q':[.1, .2, .3, .4]}
+vocab['UNK'] = np.random.random(300)
 
 def build_word(tokens, vocab):
     word_vector = []
-    a = []
     for i in range(len(tokens)):
+        a = []
         if (tokens[i] in vocab):
             if (word_vector == []):
                 word_vector = np.hstack((word_vector, vocab.get(tokens[i])))
@@ -29,8 +29,13 @@ def build_word(tokens, vocab):
                         else:
                             a = np.add(a, vocab.get(tokens[i][j]))
                     else:
-                        a = np.mean([a, vocab.get('UNK')], axis=0)
+                        if (a == []):
+                            a = vocab.get('UNK')
+                        else:
+                            a = np.add(a, vocab.get('UNK'))
                 a = a/len(tokens[i])
-                print(a)
+            if (word_vector == []):
+                word_vector = np.hstack((word_vector, a))
+            else:
                 word_vector = np.vstack((word_vector, a))
     return word_vector
